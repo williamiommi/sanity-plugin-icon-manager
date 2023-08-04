6418bf4e0c0df7fe712efa37d01ad8a9b6b23aa1
William Iommi <william.iommi@gmail.com>
2023-08-04 21:07:05 +0200

    chore: first commit w/ sanity-plugin-kit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {definePlugin} from 'sanity'
+
+interface MyPluginConfig {
+  /* nothing here yet */
+}
+
+/**
+ * Usage in `sanity.config.ts` (or .js)
+ *
+ * ```ts
+ * import {defineConfig} from 'sanity'
+ * import {myPlugin} from 'sanity-plugin-enhanced-groups'
+ *
+ * export default defineConfig({
+ *   // ...
+ *   plugins: [myPlugin()],
+ * })
+ * ```
+ */
+export const myPlugin = definePlugin<MyPluginConfig | void>((config = {}) => {
+  // eslint-disable-next-line no-console
+  console.log('hello from sanity-plugin-enhanced-groups')
+  return {
+    name: 'sanity-plugin-enhanced-groups',
+  }
+})
