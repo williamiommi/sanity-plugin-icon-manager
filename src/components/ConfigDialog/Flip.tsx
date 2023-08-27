398e69718c310baad103ab2694a24ceae7bbd06b
williamiommi <william.iommi@gmail.com>
2023-08-27 21:07:27 +0200

    chore: flip transformation

---
+++ src/components/ConfigDialog/Flip.tsx
@@ -0,0 +1,44 @@
+import {Flex} from '@sanity/ui'
+import useConfigurationState from '../../hooks/useConfigurationState'
+import HeightIcon from '../icons/HeightIcon'
+import WidthIcon from '../icons/WidthIcon'
+import {StyledBaseButton} from '../shared/SharedStyledComponents'
+import {StyledHeading} from './Styled'
+
+const Flip = () => {
+  const {flipH, flipV, onClickFlipH, onClickFlipV} = useConfigurationState()
+  return (
+    <Flex
+      direction={['column', 'column', 'row']}
+      gap={[2, 2, 1]}
+      align={['flex-start', 'flex-start', 'center']}
+      style={{width: '100%'}}
+    >
+      <StyledHeading>Flip:</StyledHeading>
+      <Flex flex={1} gap={1} style={{width: '100%'}}>
+        <StyledBaseButton
+          icon={<WidthIcon width={15} height={15} />}
+          title='Horizontal'
+          mode={`${flipH ? 'default' : 'ghost'}`}
+          tone='primary'
+          fontSize={1}
+          padding={2}
+          onClick={onClickFlipH}
+          style={{width: '100%'}}
+        />
+        <StyledBaseButton
+          icon={<HeightIcon width={15} height={15} />}
+          title='Vertical'
+          mode={`${flipV ? 'default' : 'ghost'}`}
+          tone='primary'
+          fontSize={1}
+          padding={2}
+          onClick={onClickFlipV}
+          style={{width: '100%'}}
+        />
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Flip
