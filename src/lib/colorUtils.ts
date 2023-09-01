a65c2d6977e2b4cf43411c39c7d4967a1470e436
williamiommi <william.iommi@gmail.com>
2023-09-01 11:49:38 +0200

    chore: minor to color utils

--- src/lib/colorUtils.ts
+++ src/lib/colorUtils.ts
@@ -1,41 +1,16 @@
+import {colorToString, stringToColor} from '@iconify/utils'
+import {RGBColor} from '@iconify/utils/lib/colors/types'
 import {RgbaColor} from 'react-colorful'
 
 export const HEX_BLACK = '#000000'
 
-const round = (number: number, digits = 0, base = Math.pow(10, digits)): number => {
-  return Math.round(base * number) / base
-}
-
-const format = (number: number) => {
-  const hex = number.toString(16)
-  return hex.length < 2 ? `0${hex}` : hex
-}
-
 export const rgbaToHex = ({r, g, b, a}: RgbaColor): string => {
-  if (r === undefined || g === undefined || b === undefined || a === undefined) return '#000000'
-  const alphaHex = a < 1 ? format(round(a * 255)) : ''
-  return `#${format(r)}${format(g)}${format(b)}${alphaHex}`
+  const color = colorToString({type: 'rgb', r, g, b, alpha: a})
+  return color || ''
 }
 
 export const hexToRgba = (hex: string): RgbaColor => {
-  const reg = /^#{0,1}[0-9A-F]{6}[0-9a-f]{0,2}$/i
-  if (!hex || !reg.test(hex)) return {r: 0, g: 0, b: 0, a: 1}
-
-  if (hex[0] === '#') hex = hex.substring(1)
-
-  if (hex.length < 6) {
-    return {
-      r: parseInt(hex[0] + hex[0], 16),
-      g: parseInt(hex[1] + hex[1], 16),
-      b: parseInt(hex[2] + hex[2], 16),
-      a: hex.length === 4 ? round(parseInt(hex[3] + hex[3], 16) / 255, 2) : 1,
-    }
-  }
-
-  return {
-    r: parseInt(hex.substring(0, 2), 16),
-    g: parseInt(hex.substring(2, 4), 16),
-    b: parseInt(hex.substring(4, 6), 16),
-    a: hex.length === 8 ? round(parseInt(hex.substring(6, 8), 16) / 255, 2) : 1,
-  }
+  const color = stringToColor(hex) as RGBColor
+  if (color) return {r: color.r, g: color.g, b: color.b, a: color.alpha}
+  return {r: 0, g: 0, b: 0, a: 1}
 }
