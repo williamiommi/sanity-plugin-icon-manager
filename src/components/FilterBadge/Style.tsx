81deae0d3321f6f35ec3e0d68480e54652a9e632
williamiommi <william.iommi@gmail.com>
2023-09-04 22:32:07 +0200

    chore: minor

--- src/components/FilterBadge/Style.tsx
+++ src/components/FilterBadge/Style.tsx
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 
 export const StyledFilterBadge = styled(Badge)`
   position: absolute;
-  display: flex;
+  display: flex !important;
   align-items: center;
   justify-content: center;
-  top: -5;
-  right: -5;
+  top: -5px;
+  right: -5px;
   z-index: 1;
   border-radius: 50%;
-  width: 20;
-  height: 20;
+  width: 20px;
+  height: 20px;
   padding: 0;
 `
