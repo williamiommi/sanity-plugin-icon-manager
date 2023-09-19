ed757d6e19bd0af958e10b7a237a06ccef4939b3
williamiommi <william.iommi@gmail.com>
2023-09-19 16:00:23 +0200

    chore: preview revisited to satisfy portable text inline and block icons

--- src/components/IconifyPreviewComponent/index.tsx
+++ src/components/IconifyPreviewComponent/index.tsx
@@ -1,4 +1,3 @@
-import {Card, Flex, Text, Tooltip} from '@sanity/ui'
 import {PreviewProps} from 'sanity'
 import {IconifyType} from '../../types/IconifyType'
 import IconPreview from '../IconPreview'
@@ -8,26 +7,18 @@ export type IconifyPreviewComponentProps = PreviewProps & IconifyType
 const IconifyPreviewComponent = (props: IconifyPreviewComponentProps) => {
   if (props.isPlaceholder || !props.icon) return props.renderDefault(props)
 
-  return (
-    <Tooltip
-      portal
-      content={
-        <Card padding={2}>
-          <Text size={1}>{props.icon}</Text>
-        </Card>
-      }
-    >
-      <Flex
-        padding={2}
-        gap={3}
-        align='center'
-        justify='center'
-        style={{...(props.metadata.color && {color: props.metadata.color.hex})}}
-      >
-        <IconPreview value={{icon: props.icon, metadata: props.metadata}} width={40} height={40} />
-      </Flex>
-    </Tooltip>
-  )
+  return props.renderDefault({
+    ...props,
+    title: props.icon,
+    media: (
+      <IconPreview
+        value={{icon: props.icon, metadata: props.metadata}}
+        width='80%'
+        height='80%'
+        hideText
+      />
+    ),
+  })
 }
 
 export default IconifyPreviewComponent
